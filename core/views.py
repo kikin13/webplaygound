7d1bbffcea334e7552332f829946f7ddbfbc9c17
Enrique Cervantes <cervtsluis13@gmail.com>
2020-06-01 22:19:56 -0500

    Configurando App pages y pagina de pages y details

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.shortcuts import render 
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
 
-def sample(request):
-    return render(request, "core/sample.html")
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':"Mi web"})
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
+
