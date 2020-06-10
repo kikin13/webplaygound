e74cb24cbbbcba0dd7551519e1f31571ff88a7b6
Enrique Cervantes <cervtsluis13@gmail.com>
2020-06-10 16:52:52 -0500

    Registro CBV(Vistas Basadas en Clases)Terminado

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Path de autentificación
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
