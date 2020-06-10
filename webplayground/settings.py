47a3cb207cd573f72881af90e8f5dd7818031ba8
Enrique Cervantes <cervtsluis13@gmail.com>
2020-06-10 12:21:10 -0500

    Login y logout terminados

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Redireccionamiento de autentificacion 
+LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    #Path de autentificación
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
